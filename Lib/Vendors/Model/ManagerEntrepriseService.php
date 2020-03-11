e5a26209248a0326bcc6308688c2c766b545d980
voahirana <voahiraniainaraveloherisoa@gmail.com>
2020-03-11 18:19:55 +0100

    Création d'un fichier ManagerEntrepriseService

---
+++ Lib/Vendors/Model/ManagerEntrepriseService.php
@@ -0,0 +1,113 @@
+<?php
+    
+    /**
+     * Manager de l'entité EntrepriseService
+     *
+     * @author Voahirana
+     *
+     * @since 11/03/20 
+     */
+
+    namespace Model;
+
+    use \Core\DbManager;
+    use \Entity\EntrepriseService;
+
+    class ManagerEntrepriseService extends DbManager
+    {
+       /**
+         * Lister les services d'une entreprise
+         *
+         * @param array $attributes Critères des données à lister
+         *
+         * @return array
+         */
+        public function lister($attributes) 
+        {
+            $entrepriseServices = array();
+            $resultats          = $this->findAll('entreprise_service', $attributes);
+            if (!empty($resultats)) {
+                foreach ($resultats as $resultat) {
+                    $entrepriseService    = new entrepriseService($resultat);
+                    $entrepriseServices[] = $entrepriseService;
+                }
+            }            
+            return $entrepriseServices;
+        }
+
+         /** 
+         * Créer un objet vide
+         *
+         * @return object
+         */
+        public function initialiser()
+        {
+            return new EntrepriseService();
+        }
+
+        /** 
+         * Chercher un service d'une entreprise
+         *
+         * @param array $parameters Critères des données à chercher
+         *
+         * @return object|empty
+         */
+        public function chercher($parameters)
+        {
+            $fields            = array();
+            $values            = array();
+            $entrepriseService = "";
+            foreach ($parameters as $key => $value) {
+                if (filter_var($value, FILTER_VALIDATE_INT) == true) {
+                    $value = $value;
+                } else {
+                    $value =  "'" . $value . "'";
+                }
+                $fields [] = $key;
+                $values [] = $value;
+            }            
+            $resultat = $this->findOne('entreprise_service', $fields, $values);
+            if (!empty($resultat)) {
+                $entrepriseService = new EntrepriseService($resultat);
+            }
+            return $entrepriseService;
+        }
+
+        /**
+         * Ajouter un service d'une entreprise
+         *
+         * @param array $parameters Les données à ajouter
+         *
+         * @return object
+         */
+        public function ajouter($parameters) 
+        { 
+            $parameters['idEntrepriseService'] = $this->insert('entreprise_service', $parameters);
+            return new EntrepriseService($parameters);
+        }
+
+        /**
+         * Modifier un service d'une entreprise
+         *
+         * @param array $parameters Les données à modifier
+         *
+         * @return object
+         */
+        public function modifier($parameters) 
+        {
+            $this->update('entreprise_service', $parameters);
+            return new EntrepriseService($parameters);
+        }
+
+        /**
+         * Supprimer un service d'une entreprise
+         *
+         * @param array $parameters Critères des données à supprimer
+         *
+         * @return empty
+         */
+        public function supprimer($parameters) 
+        {
+            return $this->delete('entreprise_service', $parameters);
+        }
+    }
