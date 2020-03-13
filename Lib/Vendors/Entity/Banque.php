2b08bf5cdb3e4c2292d4e321677fa32641187d9a
voahirana <voahiraniainaraveloherisoa@gmail.com>
2020-03-13 15:49:23 +0100

    Création de l'entité Banque

---
+++ Lib/Vendors/Entity/Banque.php
@@ -0,0 +1,103 @@
+<?php 
+	
+	/** 
+	 * Entité Banque
+	 *
+	 * @author Voahirana 
+	 *
+	 * @since 13/03/20
+	 */
+
+	namespace Entity;
+
+	class Banque
+	{
+		private $idBanque;
+		private $codeBanque;
+		private $nomBanque;
+		private $ville;
+		
+		/** 
+		 * Initialisation d'un Banque
+		 *
+		 * @param array $data Données à intialiser 
+		 *
+		 * @return empty
+		 */
+	    public function __construct($data = array())
+	    {
+	        if(!empty($data)) {
+	        	$this->hydrate($data);
+	        }	            
+	    }
+
+	    /** 
+		 * Remplir la structure d'un objet 
+		 *
+		 * @param array $data Données à remplir
+		 *
+		 * @return empty
+		 */
+	    public function hydrate($data)
+	    {
+	        foreach ($data as $attribut => $data) {
+	            $method = 'set'.str_replace(' ', '', ucwords(str_replace('_', ' ', $attribut)));
+	            if (is_callable(array($this, $method))) {
+	                $this->$method($data);
+	            }
+	        }
+	    }
+
+	    /** 
+	     * Convertir un objet en tableau 
+	     *
+	     * @return array
+	     */
+	    public function toArray()
+	    {
+	    	return get_object_vars($this);
+	    }
+
+	// Getters
+		public function getIdBanque()
+		{
+			return $this->idBanque;
+		}
+
+		public function getCodeBanque()
+		{
+			return $this->codeBanque;
+		}
+
+		public function getNomBanque()
+		{
+			return $this->nomBanque;
+		}
+
+		public function getVille()
+		{
+			return $this->ville;
+		}
+
+	// Seters
+		public function setIdBanque($idBanque)
+		{
+			$this->idBanque = $idBanque;
+		}
+
+		public function setCodeBanque($codeBanque)
+		{
+			$this->codeBanque = $codeBanque;
+		}
+
+		public function setNomBanque($nomBanque)
+		{
+			$this->nomBanque = $nomBanque;
+		}
+
+		public function setVille($ville)
+		{
+			$this->ville = $ville;
+		}
+		
+	}
